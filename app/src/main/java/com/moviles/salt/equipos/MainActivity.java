3cf2226a1746d39207d34fe443225e7e032cbdd7
haurbano <h.a.u.r1993@gmail.com>
2015-09-24 10:11:00 -0500

    Menu contextual implementado, AlertDialog Iinicial

--- app/src/main/java/com/moviles/salt/equipos/MainActivity.java
+++ app/src/main/java/com/moviles/salt/equipos/MainActivity.java
@@ -1,18 +1,28 @@
 package com.moviles.salt.equipos;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
 
     ListView list;
     String[] data;
+    List<String> dataEquipo;
     ArrayAdapter<String> adapter;
+    int pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +31,22 @@ public class MainActivity extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.listView);
         data = getResources().getStringArray(R.array.Equipos);
+
+        dataEquipo = new ArrayList<String>();
+        for(int i=0;i<data.length;i++)
+        {
+            dataEquipo.add(data[i]);
+        }
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
-                data);
+                dataEquipo);
         list.setAdapter(adapter);
+        registerForContextMenu(list);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -53,5 +70,49 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_ctx_list,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.
+                AdapterContextMenuInfo) item.getMenuInfo();
+        pos = info.position;
+
+
+        switch (item.getItemId())
+        {
+            case R.id.action_edit:
+                Toast.makeText(this,
+                        "Seleccionaste editar",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_delete:
+                showAlertDelete();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void showAlertDelete() {
+
+        AlertDialog alert = new AlertDialog.Builder(this).
+                setTitle("Eliminar Equipo").
+                setMessage("¿Estás seguro que deseas eliminar este equipo?").
+                setPositiveButton("Aceptar",this).
+                setNegativeButton("Cancelar",this).create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
+        if(which== DialogInterface.BUTTON_POSITIVE)
+        {}
+
     }
 }
